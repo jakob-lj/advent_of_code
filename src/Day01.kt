db9c3aa0b28339b63bd2114afa9cd61c35c9222b
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-12-03 10:15:28 +0100

    finish: d1p1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,34 @@
 fun main() {
 
-    throw IllegalStateException("Didnt do day 1 :'( ")
+    val testInput = """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """.trimIndent()
+
+    val input = testInput.split("\n")
+
+    val maxCalories = input.fold(mutableListOf<Int>()) { acc, s ->
+        if (acc.size == 0) {
+            acc.add(s.toInt())
+        } else if (s == "") {
+            acc.add(0)
+        } else {
+            acc[acc.size - 1] += s.toInt()
+        }
+        acc
+    }.max()
+
+    println(maxCalories)
 }
