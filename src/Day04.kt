739211481e6fed1ebcccfdb45ea9186eb68d337f
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-12-04 17:42:41 +0100

    finish: d4p2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,12 @@
 fun main() {
 
-    val input = readInput("day4")
+    val input = readInput("day4_test")
+
+    part1(input)
+    part2(input)
+}
+
+fun part1(input: List<String>) {
 
     val data = input.map {
         val compartments = it.split(",")
@@ -15,6 +21,23 @@ fun main() {
         (first.first <= second.first && first.second >= second.second) || (first.first >= second.first && first.second <= second.second)
     }
 
+    println(data)
+}
+
+fun part2(input: List<String>) {
+
+    val data = input.map {
+        val compartments = it.split(",")
+        val first = compartments[0].split("-")
+        val second = compartments[1].split("-")
+
+        Pair(Pair(first[0].toInt(), first[1].toInt()), Pair(second[0].toInt(), second[1].toInt()))
+    }.count {
+        val first = it.first
+        val second = it.second
+
+        first.second <= second.first && first.first >= second.first || second.first <= first.second && second.second >= first.first
+    }
 
     println(data)
 }
