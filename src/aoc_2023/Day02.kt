dfef77268c33a6b10a5078bdcc016098efdb0f0c
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-12-02 10:56:14 +0100

    feat: day 2

--- src/aoc_2023/Day02.kt
+++ src/aoc_2023/Day02.kt
@@ -1,17 +1,11 @@
 package aoc_2023
 
-fun formatInput(input: String): List<String> = input.split("\n")
+import java.lang.RuntimeException
 
-fun part1(input: List<String>) {
+data class Round(val blue: Int?, val green: Int?, val red: Int?)
+data class Game(val consumesTotalDrawings: Round, val gameId: Int)
 
-    print(input)
-}
-
-fun main() {
-
-    part1(formatInput(testInput))
-
-    val testInput = """
+private val testInput = """
     Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -19,8 +13,71 @@ fun main() {
     Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """.trimIndent()
 
-    val realInput = """
-    
+private val realInput = """
 """.trimIndent()
+
+private fun formatInput(input: String): List<String> = input.split("\n")
+
+private fun List<Round>.getMax(gameId: Int): Game {
+    val maxBlue = this.maxOf { it.blue ?: 0 }
+    val maxRed = this.maxOf { it.red ?: 0 }
+    val maxGreen = this.maxOf { it.green ?: 0 }
+
+    return Game(Round(blue = maxBlue, red = maxRed, green = maxGreen), gameId)
+}
+
+private fun formatRoundString(game: String): Pair<List<Round>, Int> {
+
+    val indexOfColon = game.indexOf(":")
+
+    val gameId = game.subSequence(5, indexOfColon).toString().toInt() // Every game starts with Game xx:
+
+    val rounds = game.subSequence((indexOfColon + 2)..game.lastIndex).split("; ")
+
+    val roundRounds = rounds.map { round ->
+        val counts = mutableMapOf<String, Int?>("green" to null, "red" to null, "blue" to null)
+
+        for (balDraw in round.split(", ")) {
+            val (ballCountString: String, color: String) = balDraw.split(" ")
+            val ballCount: Int = ballCountString.toInt()
+
+            if (color !in counts) {
+                throw RuntimeException("Cound not find color: $color")
+            }
+            counts[color] = ballCount
+        }
+
+        Round(blue = counts["blue"], green = counts["green"], red = counts["red"])
+    }
+
+    return Pair(roundRounds, gameId)
+}
+
+private fun part1(input: List<String>): Int {
+
+    val allowedGames = input.map { formatRoundString(it) }.map { it.first.getMax(it.second) }.filter { game ->
+        (game.consumesTotalDrawings.blue ?: 0) <= 14 &&
+                (game.consumesTotalDrawings.green ?: 0) <= 13 &&
+                (game.consumesTotalDrawings.red ?: 0) <= 12
+    }
+
+    return allowedGames.sumOf { it.gameId }
+}
+
+private fun part2(input: List<String>): Int {
+
+    return input.map { formatRoundString(it) }.map { it.first.getMax(it.second) }
+        .sumOf {
+            val minimums = it.consumesTotalDrawings
+
+            (minimums.blue ?: 1) * (minimums.green ?: 1) * (minimums.red ?: 1)
+        }
+}
+
+fun main() {
+
+    val result = part2(formatInput(testInput))
+
+    println(result)
 
 }
