3cedf2802e4af1355fc1033af6e49773fd71f390
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-12-02 10:03:11 +0100

    fix: move things inside class

--- src/aoc_2023/Day01.kt
+++ src/aoc_2023/Day01.kt
@@ -1,7 +1,7 @@
 package aoc_2023
 
-
-val testInput = """
+class Day01 {
+    val testInput = """
     two1nine
     eightwothree
     abcone2threexyz
@@ -11,55 +11,60 @@ val testInput = """
     7pqrstsixteen
 """.trimIndent()
 
-val textToDigits = mapOf(
-    "one" to 1,
-    "two" to 2,
-    "three" to 3,
-    "four" to 4,
-    "five" to 5,
-    "six" to 6,
-    "seven" to 7,
-    "eight" to 8,
-    "nine" to 9
-)
+    val textToDigits = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
 
-fun String.addFirstAndLastLetter(textualDigit: String): String {
-    return "${textualDigit[0]}$this${textualDigit[textualDigit.length - 1]}"
-}
-
-fun textToDigit(line: String): String {
-    var mutableLine = line
-    for (textualDigit in textToDigits.keys) {
-        mutableLine =
-            mutableLine.replace(textualDigit, textToDigits[textualDigit].toString().addFirstAndLastLetter(textualDigit))
+    fun String.addFirstAndLastLetter(textualDigit: String): String {
+        return "${textualDigit[0]}$this${textualDigit[textualDigit.length - 1]}"
     }
 
-    return mutableLine.filter { it.isDigit() }
-}
+    fun textToDigit(line: String): String {
+        var mutableLine = line
+        for (textualDigit in textToDigits.keys) {
+            mutableLine =
+                mutableLine.replace(
+                    textualDigit,
+                    textToDigits[textualDigit].toString().addFirstAndLastLetter(textualDigit)
+                )
+        }
 
-enum class Part {
-    ONE,
-    TWO
-}
-
-fun part1(part: Part): Int {
-
-    val data = realInput
-
-    val linesWithNumbers = data.split("\n")
-        .map { row -> if (part == Part.ONE) row.filter { it.isDigit() } else textToDigit(row) }
-
-    val result = linesWithNumbers.map {
-        "${it[0]}${
-            it[it.length - 1]
-        }".toInt()
+        return mutableLine.filter { it.isDigit() }
     }
 
-    return result.sum()
+    enum class Part {
+        ONE,
+        TWO
+    }
+
+    fun part1(part: Part): Int {
+
+        val data = realInput
+
+        val linesWithNumbers = data.split("\n")
+            .map { row -> if (part == Part.ONE) row.filter { it.isDigit() } else textToDigit(row) }
+
+        val result = linesWithNumbers.map {
+            "${it[0]}${
+                it[it.length - 1]
+            }".toInt()
+        }
+
+        return result.sum()
+    }
+
 }
 
 fun main() {
-    println(part1(Part.TWO))
+    println(Day01().part1(Day01.Part.TWO))
 }
 
 
